42ed22a9cb5f1da114afc0ebcff87455bdb3615c
jazkamer <v.rodionov@protonmail.com>
2023-03-23 01:14:51 +0200

    feat: add split logic

---
+++ cmd/split/stores.go
@@ -0,0 +1,11 @@
+package split
+
+import (
+	filesystem "github.com/wealdtech/go-eth2-wallet-store-filesystem"
+	types "github.com/wealdtech/go-eth2-wallet-types/v2"
+)
+
+func createStore(path string) (store types.Store) {
+	store = filesystem.New(filesystem.WithLocation(path))
+	return
+}
